a243c519787a88dfe9e5d9fab287b0bd1ca576d7
Jan Medrek <jan.medrek@sap.com>
2019-02-04 18:38:40 +0100

    Update formula class name

--- one-click-integration.rb
+++ one-click-integration.rb
@@ -1,12 +1,12 @@
 # Documentation: https://docs.brew.sh/Formula-Cookbook
 #                https://www.rubydoc.info/github/Homebrew/brew/master/Formula
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-class OneClickIntegrationScript < Formula
+class OneClickIntegration < Formula
   desc ""
   homepage ""
   url "https://github.com/janmedrek/one-click-integration-script/archive/v1.0.0.tar.gz"
   sha256 "ba58cc709cf5c6455e3bd63cf598f64169e62cf38d7db53a83ecf01ec9dd36a8"
-  
+
   depends_on "jq"
   depends_on "curl"
   depends_on "openssl"
