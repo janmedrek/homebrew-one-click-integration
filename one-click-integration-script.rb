b4a47058b11c740efcf990273f6c4092612efa81
Jan Medrek <jan.medrek@sap.com>
2019-02-04 18:33:49 +0100

    Add formula for one-click-integration script

---
+++ one-click-integration-script.rb
@@ -0,0 +1,19 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://www.rubydoc.info/github/Homebrew/brew/master/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class OneClickIntegrationScript < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/janmedrek/one-click-integration-script/archive/v1.0.0.tar.gz"
+  sha256 "ba58cc709cf5c6455e3bd63cf598f64169e62cf38d7db53a83ecf01ec9dd36a8"
+  
+  depends_on "jq"
+  depends_on "curl"
+  depends_on "openssl"
+  depends_on "base64"
+  depends_on "kubectl" => :recommended
+
+  def install
+    bin.install "one-click-integration"
+  end
+end
