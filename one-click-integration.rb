1c611663fb5bfb0a361c87549fa35ed1efea3e1f
Jan Medrek <jan.medrek@sap.com>
2019-02-04 18:40:48 +0100

    Update installation for formula

--- one-click-integration.rb
+++ one-click-integration.rb
@@ -14,6 +14,6 @@ class OneClickIntegration < Formula
   depends_on "kubectl" => :recommended
 
   def install
-    bin.install "one-click-integration-script"
+    bin.install "one-click-integration-script.sh"
   end
 end
